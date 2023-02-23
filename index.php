8d1d2f11db17ad73e759c67c24c8bdbc2c5cd226
Bjarni Guðmann <89957113+BjarniGod@users.noreply.github.com>
2023-02-22 23:42:29 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+require('model/database.php');
+require('model/todo_db.php');
+
+$item_number = filter_input(INPUT_POST, "item_number", FILTER_VALIDATE_INT);
+$title = filter_input(INPUT_POST, "item_number", FILTER_UNSAFE_RAW);
+$description = filter_input(INPUT_POST, "item_number", FILTER_UNSAFE_RAW);
+
+
+
+$action = filter_input(INPUT_POST, "action", FILTER_UNSAFE_RAW);
+if(!$action) {
+    $action = filter_input(INPUT_GET, "action", FILTER_UNSAFE_RAW);
+    if(!$action) {
+        $action = 'list_todos';
+    }
+}
+
+switch($action) {
+    case "list_todos":
+        $todos = get_todos();
+        include('view/todo_list.php');
+        break;
+
+    case "add_todo":
+        if($title && $description) {
+        add_course($title, $description);
+        header("Location: .?action=list_courses");
+        } else {
+            $error = "Invalid assignment data. Check all the fields and try again!";
+            include('view/error.php');
+            exit();
+        }
+    
+    case "delete_todo":
+        if($item_number) {
+            delete_course($item_number);
+            header("Location: .?action=list_todos");
+        } else {
+            $error = "Missing or incorrect item number!";
+            include('view/error.php');
+        }
+        break;
+    
+
+
+    default:
+        $title = get_todo_title($course_id);
+        $todos = get_todos();
+        include('view/todo_list.php');
+}
+
+?>
